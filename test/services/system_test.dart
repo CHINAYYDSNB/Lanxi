3ba4cb71734ffdfe7ae30d9586f0087ebec91211
LanxiFurry <140574312+CHINAYYDSNB@users.noreply.github.com>
2026-07-25 00:28:47 +0800

    test: fix SystemParser test to use sectioned <<<SECTION>>> format

--- test/services/system_test.dart
+++ test/services/system_test.dart
@@ -3,23 +3,35 @@ import 'package:lanxi/services/system.dart';
 import 'package:lanxi/models/system_info.dart';
 
 void main() {
-  test('SystemParser.parse produces a sane SystemInfo from top output', () {
+  test('SystemParser.parse produces a sane SystemInfo from sectioned output', () {
     const raw = '''
-top - 10:00:00 up 1 day,  1:00,  1 user,  load average: 0.50, 0.40, 0.30
-Tasks: 100 total,   2 running
-%Cpu(s): 12.3 us,  5.0 sy,  0.0 ni, 82.7 id
-%Cpu0  : 10.0 us,  0.0 sy
-%Cpu1  : 20.0 us,  0.0 sy
-KiB Mem :  8000000 total,  4000000 free,  3000000 used,  1000000 buff/cache
-KiB Swap:  1000000 total,   900000 free,   100000 used
+<<<LOAD>>>
+0.50 0.40 0.30
+<<<CPU>>>
+us 12.3 sy 5.0 ni 0.0 id 82.7 wa 0.0 hi 0.0 si 0.0 st 0.0
+<<<CPUINFO>>>
+cpu cores : 4
+model name : Intel Core
+<<<MEM>>>
+Mem: 8000000 3000000 4000000 1000000
+<<<DISK>>>
+2000000 1900000 100000
+<<<UPTIME>>>
+12345
+<<<HOSTNAME>>>
+myhost
+<<<KERNEL>>>
+5.15.0
+<<<OS>>>
+Ubuntu
 ''';
 
     final info = SystemParser.parse(raw);
 
     expect(info, isA<SystemInfo>());
-    expect(info.cpuCores, greaterThan(0));
+    expect(info.cpuCores, 4);
     expect(info.cpuUsage, isA<double>());
-    expect(info.memoryTotal, greaterThan(0));
-    expect(info.load1, isA<double>());
+    expect(info.memoryTotal, 8000000);
+    expect(info.load1, closeTo(0.5, 0.001));
   });
 }
